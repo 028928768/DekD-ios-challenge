5b57d3fc6f2bf7468b2a7ec505027cb238e0e944
Simon <kantachat.puagkajee@7peakssoftware.com>
2023-11-06 12:52:52 +0700

    Initial Commit

---
+++ DekD-ios-challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DekD-ios-challenge
+//
+//  Created by Simon Siwell on 6/11/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DekD-ios-challenge/DekD_ios_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DekD_ios_challengeApp.swift
+//  DekD-ios-challenge
+//
+//  Created by Simon Siwell on 6/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct DekD_ios_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
