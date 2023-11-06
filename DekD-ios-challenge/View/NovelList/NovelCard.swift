d9c099a4a5a146f817fd2c698297bdfb788c5849
Simon <kantachat.puagkajee@7peakssoftware.com>
2023-11-06 13:59:38 +0700

    added layout for novelListView

---
+++ DekD-ios-challenge/View/NovelList/NovelCard.swift
@@ -0,0 +1,18 @@
+//
+//  NovelCard.swift
+//  DekD-ios-challenge
+//
+//  Created by Simon Siwell on 6/11/2566 BE.
+//
+
+import SwiftUI
+
+struct NovelCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NovelCard()
+}
---
+++ DekD-ios-challenge/View/NovelList/NovelListView.swift
@@ -0,0 +1,18 @@
+//
+//  NovelListView.swift
+//  DekD-ios-challenge
+//
+//  Created by Simon Siwell on 6/11/2566 BE.
+//
+
+import SwiftUI
+
+struct NovelListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NovelListView()
+}
