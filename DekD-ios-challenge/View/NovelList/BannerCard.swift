0c85bff075681807cdc247606b61f19d549fcb25
Simon <kantachat.puagkajee@7peakssoftware.com>
2023-11-06 15:48:54 +0700

    added noteCard prototype cell

---
+++ DekD-ios-challenge/View/NovelList/BannerCard.swift
@@ -0,0 +1,18 @@
+//
+//  BannerCard.swift
+//  DekD-ios-challenge
+//
+//  Created by Simon Siwell on 6/11/2566 BE.
+//
+
+import SwiftUI
+
+struct BannerCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BannerCard()
+}
